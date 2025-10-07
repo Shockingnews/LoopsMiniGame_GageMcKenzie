3cc8857d3c10cea21c1fff5bbf6330b104b12b22
Shockingnews <gagemckenzie70@gmail.com>
2025-10-07 15:45:19 -0300

    movable player

---
+++ LoopsMiniGame_GageMcKenzie/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsMiniGame_GageMcKenzie
+{
+    internal class Program
+    {
+
+        static int x;
+        static int y;
+        static bool gameOver = false;
+        static void Main(string[] args)
+        {
+            PlayerDraw(10, 10);
+           while(gameOver == false)
+            {
+                
+                PlayerUpdate();
+                
+            }
+            
+        }
+        static void PlayerDraw(int startX, int startY)
+        {
+            Console.SetCursorPosition(startX, startY);
+            Console.Write("G");
+            x = startX;
+            y = startY;
+        }
+        static void PlayerUpdate()
+        {
+            x = x;
+            y = y;
+            
+            ConsoleKeyInfo lastSletion =new ConsoleKeyInfo();
+            lastSletion = Console.ReadKey(true);
+            if (lastSletion.Key == ConsoleKey.W)
+            {
+
+                Console.SetCursorPosition(x,y -= 1);
+                Console.Write("G");
+                if (y > 0)
+                {
+                    Console.SetCursorPosition(x, y + 1);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.Write(" ");
+                }
+            }
+            else if (lastSletion.Key == ConsoleKey.S)
+            {
+                Console.SetCursorPosition(x,y+=1);
+                Console.Write("G");
+                if (y > 0)
+                {
+                    Console.SetCursorPosition(x, y - 1);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.Write(" ");
+                }
+            }
+            else if (lastSletion.Key == ConsoleKey.A)
+            {
+                Console.SetCursorPosition(x -=1 , y);
+                Console.Write("G");
+                if (x > 0)
+                {
+                    Console.SetCursorPosition(x+1, y);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.Write(" ");
+                }
+            }
+            else if (lastSletion.Key == ConsoleKey.D)
+            {
+                Console.SetCursorPosition(x+=1, y);
+                Console.Write("G");
+                if (x > 0)
+                {
+                    Console.SetCursorPosition(x- 1, y);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.Write(" ");
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Game Over");
+                gameOver = true;
+            }
+        }
+    }
+}
